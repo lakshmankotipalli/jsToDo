9fff54fc963043d419ea12e6b9d72ba3eaf7070e
Lakshman <lakshman.kotipalli@outlook.com>
2018-03-24 02:02:50 +0530

    to do logic and building ul

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var toDoObj = {todo:'', completed: false};
+document.body.onload = addElement;
+
+var toDoObj = {};
 var toDoList = [];
 
 function addToDo() {
@@ -9,15 +11,29 @@ function addToDo() {
         toDoObj = {};
         console.log(toDoList);
         document.getElementById('toDoInput').value = '';
-        toDoList.forEach(function (value, index) {
-            console.log(value);
-            document.getElementById('addedToDo').innerHTML = value.todo;
-        });
+        createToDoList(toDoList);
     } else {
         alert('Nothing to do? Give it a thought!!');
     }
-    
 }
 
+    var listElement = createToDoList(toDoList);
+    document.getElementById('board').appendChild(listElement);
 
+function createToDoList (toDoList) {
+    var list = document.createElement('ul');
+    for(var i=0; i<toDoList.length;i++) {
+        var listItem = document.createElement('li');
+        listItem.appendChild(document.createTextNode(toDoList[i].todo));
+        list.appendChild(listItem);
+    }
+    return list;
+}
 
+function addElement () {
+    var heading = document.createElement("h2");
+    var txt = document.createTextNode("ToDo App with JS");
+    heading.appendChild(txt);
+    var currentDiv = document.getElementById('dabba');
+    document.body.insertBefore(heading, currentDiv);
+}
