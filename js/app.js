6b6d28d5a85a431b68acd73794f41f2315924ca2
Lakshman <lakshman.kotipalli@outlook.com>
2018-03-24 00:22:28 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-var toDoObj = {};
+var toDoObj = {todo:'', completed: false};
 var toDoList = [];
 
 function addToDo() {
-    toDoObj.todo = document.getElementById('toDoInput').value;
-    toDoObj.completed = false;
-    toDoList.push(toDoObj);
-    toDoObj = {};
-    console.log(toDoList);
-    document.getElementById('toDoInput').value = '';
-    toDoList.forEach(function (value, index) {
-        console.log(value);
-        document.getElementById('addedToDo').innerHTML = value.todo;
-    });
+    if(document.getElementById('toDoInput').value != "") {
+        toDoObj.todo = document.getElementById('toDoInput').value;
+        toDoObj.completed = false;
+        toDoList.push(toDoObj);
+        toDoObj = {};
+        console.log(toDoList);
+        document.getElementById('toDoInput').value = '';
+        toDoList.forEach(function (value, index) {
+            console.log(value);
+            document.getElementById('addedToDo').innerHTML = value.todo;
+        });
+    } else {
+        alert('Nothing to do? Give it a thought!!');
+    }
+    
 }
 
 
